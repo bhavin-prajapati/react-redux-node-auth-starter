0d2edee7f26c3912f89e5a15c9d0b6702d2dddbc
Bhavin Prajapati <bprajapati@tribalscale.com>
2018-04-10 00:57:21 -0400

    [BP] Added AuthRoute tests

--- client/src/components/DashboardPage/DashboardPage.test.js
+++ client/src/components/DashboardPage/DashboardPage.test.js
@@ -8,13 +8,56 @@ configure({ adapter: new Adapter() });
 describe('Dashboard Page', () => {
   let subject;
   let history;
+  let mockGetUser;
+  let mockLoadDashboard;
 
-  beforeEach(() => {
-    history = { push: () => { } };
-    subject = shallow(<DashboardComponent history={history} />);
+  describe('Load dashboard', () => {
+    beforeEach(() => {
+      history = { push: () => { } };
+      mockGetUser = jest.fn();
+      mockLoadDashboard = jest.fn();
+      subject = shallow(<DashboardComponent
+        history={history}
+        getUser={mockGetUser}
+        loadDashboard={mockLoadDashboard}
+      />);
+    });
+
+    it('Render', () => {
+      expect(subject).toHaveLength(1);
+    });
   });
 
-  it('Can Mount', () => {
-    expect(subject).toHaveLength(1);
+  describe('Render component', () => {
+    beforeEach(() => {
+      history = { push: () => { } };
+      mockGetUser = jest.fn();
+      subject = shallow(<DashboardComponent
+        history={history}
+        getUser={mockGetUser}
+        dashboard={{ data: 'testing 123' }}
+      />);
+    });
+
+    it('Render', () => {
+      expect(subject).toHaveLength(1);
+    });
+  });
+
+  describe('Get User', () => {
+    beforeEach(() => {
+      history = { push: () => { } };
+      mockGetUser = jest.fn();
+      document.cookie = '_sid=12345;max-age=60';
+      subject = shallow(<DashboardComponent
+        history={history}
+        getUser={mockGetUser}
+        dashboard={{ data: 'testing 123' }}
+      />);
+    });
+
+    it('Render', () => {
+      expect(subject).toHaveLength(1);
+    });
   });
 });
